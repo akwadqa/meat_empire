b019d9e9d5e7ffc57d059c9590d3b4e3e7fa7f15
SaraAkwad <sarak@akwad.qa>
2025-01-06 12:50:25 +0300

    Load the home API while splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,47 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:meat_empire/src/features/home/data/home_repository.dart';
 import 'src/app.dart';
 import 'src/features/auth/application/auth_service.dart';
 import 'src/riverpod_observer.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-  final container = ProviderContainer(observers: [RiverpodObserver()]);
-  await container.read(sharedPreferencesProvider.future);
-  runApp(UncontrolledProviderScope(
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  final container = await initializeProviders();
+  await handleSplashScreen(container);
+
+  runApp(
+    UncontrolledProviderScope(
       container: container,
       child: EasyLocalization(
-          supportedLocales: [Locale('en'), Locale('ar')],
-          path: 'assets/translations',
-          child: const App())));
+        supportedLocales: const [Locale('en'), Locale('ar')],
+        path: 'assets/translations',
+        child: const App(),
+      ),
+    ),
+  );
+}
+
+Future<ProviderContainer> initializeProviders() async {
+  final container = ProviderContainer(observers: [RiverpodObserver()]);
+  await container.read(sharedPreferencesProvider.future);
+  return container;
+}
+
+Future<void> handleSplashScreen(ProviderContainer container) async {
+  const minSplashDuration = 2000;
+  final startTime = DateTime.now();
+  await container.read(homeProvider.future);
+  final loadDuration = DateTime.now().difference(startTime).inMilliseconds;
+
+  if (loadDuration < minSplashDuration) {
+    await Future.delayed(Duration(milliseconds: minSplashDuration - loadDuration));
+  }
+
+  FlutterNativeSplash.remove();
 }
